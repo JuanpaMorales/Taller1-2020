cde4b99e78aa60803380858c476a7a99e811ff3c
Maximiliano <MaximilianoP90@gmail.com>
2020-02-26 13:50:29 -0300

    Se agregaron los headers y cpp de Comandos, y la funcion create.

---
+++ Taller1/comandos.cpp
@@ -0,0 +1,17 @@
+#include "comandos.h"
+
+void create(int num, char x){
+
+    if((num < 0) || (x != 88 || x != 120)){
+
+        printf("Por favor ingresa un parametro correcto");
+    }
+    else{
+
+        int *num = new int;
+        char *x = new char;
+        //deberia crear uno solo, dependiendo el parametro que se le pase
+    }
+
+
+}
---
+++ Taller1/comandos.h
@@ -0,0 +1,9 @@
+#ifndef COMANDOS_H_INCLUDED
+#define COMANDOS_H_INCLUDED
+#include <stdio.h>
+
+void create(int num, char x);
+
+
+
+#endif // COMANDOS_H_INCLUDED
