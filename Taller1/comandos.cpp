6247bccde798ce5fd447e45452edd5b24387accd
Maximiliano <MaximilianoP90@gmail.com>
2020-02-26 21:42:15 -0300

    Cambios en create

--- Taller1/comandos.cpp
+++ Taller1/comandos.cpp
@@ -2,15 +2,17 @@
 
 void create(int num, char x){
 
-    if((num < 0) || (x != 88 || x != 120)){
+    if((num < 0) || (x != 'x' x != 'X')){
 
         printf("Por favor ingresa un parametro correcto");
     }
     else{
 
-        int *num = new int;
-        char *x = new char;
-        //deberia crear uno solo, dependiendo el parametro que se le pase
+        if(num >= 0)
+            int num = new int;
+        else
+            if(x == 'x' || x == 'X')
+            char x = new char;
     }
 
 
